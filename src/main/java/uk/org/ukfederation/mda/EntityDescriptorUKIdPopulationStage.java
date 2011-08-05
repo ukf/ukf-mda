b698c207f83b81cfae5cd3162df2c1a6d3d5b948
Ian Young <ian@iay.org.uk>
2011-08-05 14:40:55 +0000

    Bugzilla 773: registered metadata should contain ID attribute matching ukNNNNNN regular expression * all items must be EntityDescriptor elements * all items must have ID attributes * all ID attribute values must be distinct * all ID attribute values must match the regular expression "^uk[0-9]{6}$"

--- src/main/java/uk/org/ukfederation/mda/EntityDescriptorUKIdPopulationStage.java
+++ src/main/java/uk/org/ukfederation/mda/EntityDescriptorUKIdPopulationStage.java
@@ -16,11 +16,22 @@
 
 package uk.org.ukfederation.mda;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import net.jcip.annotations.ThreadSafe;
+import net.shibboleth.metadata.ErrorStatus;
+import net.shibboleth.metadata.ItemMetadata;
 import net.shibboleth.metadata.dom.DomElementItem;
 import net.shibboleth.metadata.dom.saml.SamlMetadataSupport;
-import net.shibboleth.metadata.pipeline.BaseIteratingStage;
+import net.shibboleth.metadata.pipeline.BaseStage;
+import net.shibboleth.metadata.pipeline.ComponentInitializationException;
 import net.shibboleth.metadata.pipeline.StageProcessingException;
+import net.shibboleth.metadata.util.ClassToInstanceMultiMap;
 
+import org.opensaml.util.StringSupport;
 import org.opensaml.util.xml.AttributeSupport;
 import org.w3c.dom.Element;
 
@@ -28,20 +39,50 @@ import org.w3c.dom.Element;
  * A stage which, for each EntityDescriptor collection element, adds a {@link UKId}, with the entity's ID, to
  * the metadata item.
  */
-public class EntityDescriptorUKIdPopulationStage extends BaseIteratingStage<DomElementItem> {
+@ThreadSafe
+public class EntityDescriptorUKIdPopulationStage extends BaseStage<DomElementItem> {
+
+    /**
+     * Compiled regular expression.
+     */
+    private Pattern pattern;
 
     /** {@inheritDoc} */
-    protected boolean doExecute(DomElementItem item) throws StageProcessingException {
-        Element metadataElement = item.unwrap();
+    protected void doExecute(Collection<DomElementItem> items) throws StageProcessingException {
 
-        if (SamlMetadataSupport.isEntityDescriptor(metadataElement)) {
-            String id = AttributeSupport.getAttributeValue(metadataElement, null, "ID");
-            if (id != null) {
-                item.getItemMetadata().put(new UKId(id));
-            }
+        // ID values that we have already seen (they must be unique)
+        Set<String> ids = new HashSet<String>(items.size());
+        
+        for (DomElementItem item : items) {
+           Element element = item.unwrap();
+           ClassToInstanceMultiMap<ItemMetadata> metadata = item.getItemMetadata();
+           
+           if (!SamlMetadataSupport.isEntityDescriptor(element)) {
+               // all items must be EntityDescriptor elements
+               metadata.put(new ErrorStatus(getId(), "item was not an EntityDescriptor"));
+           } else {
+               String id = AttributeSupport.getAttributeValue(element, null, "ID");
+               String eid = StringSupport.nullToEmpty(AttributeSupport.getAttributeValue(element, null, "entityID"));
+               if (id == null) {
+                   metadata.put(new ErrorStatus(getId(), "EntityDescriptor " + eid + " did not have an ID attribute"));
+               } else if (!pattern.matcher(id).matches()) {
+                   metadata.put(new ErrorStatus(getId(), "EntityDescriptor " + eid + " has an ID value '" + id +
+                           "' that does not look like a UK federation identifier"));
+               } else if (ids.contains(id)) {
+                   metadata.put(new ErrorStatus(getId(), "EntityDescriptor " + eid + " has duplicate ID value " + id));
+               } else {
+                   metadata.put(new UKId(id));
+                   ids.add(id);
+               }
+           }
+           
         }
-
-        return true;
     }
     
+    /** {@inheritDoc} */
+    protected void doInitialize() throws ComponentInitializationException {
+        super.doInitialize();
+        pattern = Pattern.compile("^uk[0-9]{6}$");
+    }
+
 }

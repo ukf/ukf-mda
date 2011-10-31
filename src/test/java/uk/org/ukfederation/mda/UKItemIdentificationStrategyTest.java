bdf7b911d19859e3252f2839536bf9f31e974559
Ian Young <ian@iay.org.uk>
2011-10-31 08:29:08 +0000

    Item identification strategy for UK deployment.

---
+++ src/test/java/uk/org/ukfederation/mda/UKItemIdentificationStrategyTest.java
@@ -0,0 +1,31 @@
+
+package uk.org.ukfederation.mda;
+
+import net.shibboleth.metadata.Item;
+import net.shibboleth.metadata.ItemId;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UKItemIdentificationStrategyTest {
+
+    @Test
+    public void getItemIdentifier() {
+        UKItemIdentificationStrategy strat = new UKItemIdentificationStrategy();
+        strat.setNoItemIdIdentifier("mu");
+        
+        Item<?> item1 = new MockItem("item 1");
+        Assert.assertEquals(strat.getItemIdentifier(item1), "mu");
+        item1.getItemMetadata().put(new UKId("uk-id"));
+        Assert.assertEquals(strat.getItemIdentifier(item1), "uk-id");
+        item1.getItemMetadata().put(new ItemId("item-id"));
+        Assert.assertEquals(strat.getItemIdentifier(item1), "uk-id");
+
+        Item<?> item2 = new MockItem("item 2");
+        Assert.assertEquals(strat.getItemIdentifier(item2), "mu");
+        item2.getItemMetadata().put(new ItemId("item-id"));
+        Assert.assertEquals(strat.getItemIdentifier(item2), "item-id");
+        item2.getItemMetadata().put(new UKId("uk-id"));
+        Assert.assertEquals(strat.getItemIdentifier(item2), "uk-id");
+    }
+}

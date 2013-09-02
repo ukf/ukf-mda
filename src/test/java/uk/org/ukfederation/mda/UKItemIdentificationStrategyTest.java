3af85d1ed246c471e3dd5cfabf82d046e699959b
Ian Young <ian@iay.org.uk>
2013-09-02 14:44:03 +0100

    Extend UK item identification strategy with registrar ID.
    Additional facilities available to ignore a set of registrars, and to
    map registration authority URIs to display names for each registrar.

--- src/test/java/uk/org/ukfederation/mda/UKItemIdentificationStrategyTest.java
+++ src/test/java/uk/org/ukfederation/mda/UKItemIdentificationStrategyTest.java
@@ -1,18 +1,65 @@
 
 package uk.org.ukfederation.mda;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import net.shibboleth.metadata.ErrorStatus;
 import net.shibboleth.metadata.Item;
 import net.shibboleth.metadata.ItemId;
+import net.shibboleth.metadata.ItemMetadata;
+import net.shibboleth.metadata.dom.DomElementItem;
+import net.shibboleth.metadata.dom.saml.EntityDescriptorItemIdPopulationStage;
+import net.shibboleth.metadata.util.ClassToInstanceMultiMap;
+import net.shibboleth.utilities.java.support.xml.XMLParserException;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.w3c.dom.Element;
 
-public class UKItemIdentificationStrategyTest {
+import uk.org.ukfederation.mda.validate.mdrpi.RegistrationAuthorityPopulationStage;
 
-    @Test
-    public void getItemIdentifier() {
+public class UKItemIdentificationStrategyTest extends BaseDomTest {
+
+    private UKItemIdentificationStrategy makeStrat() {
         final UKItemIdentificationStrategy strat = new UKItemIdentificationStrategy();
         strat.setNoItemIdIdentifier("mu");
+        return strat;
+    }
+    
+    private void performExtractions(DomElementItem item) throws Exception {
+        final List<DomElementItem> items = new ArrayList<DomElementItem>();
+        items.add(item);
+        
+        final RegistrationAuthorityPopulationStage stage1 = new RegistrationAuthorityPopulationStage();
+        stage1.setId("test");
+        stage1.initialize();
+        stage1.execute(items);
+        
+        final EntityDescriptorItemIdPopulationStage stage2 = new EntityDescriptorItemIdPopulationStage();
+        stage2.setId("test");
+        stage2.initialize();
+        stage2.execute(items);
+
+        final EntityDescriptorUKIdPopulationStage stage3 = new EntityDescriptorUKIdPopulationStage();
+        stage3.setId("test");
+        stage3.initialize();
+        stage3.execute(items);   
+    }
+    
+    private DomElementItem makeItem(final String which) throws XMLParserException {
+        final String fileName = "ukidstrat/" + which + ".xml";
+        final Element doc = readXmlData(fileName);
+        return new DomElementItem(doc);
+    }
+    
+    @Test
+    public void getItemIdentifier() {
+        final UKItemIdentificationStrategy strat = makeStrat();
         
         final Item<?> item1 = new MockItem("item 1");
         Assert.assertEquals(strat.getItemIdentifier(item1), "mu");
@@ -28,4 +75,69 @@ public class UKItemIdentificationStrategyTest {
         item2.getItemMetadata().put(new UKId("uk-id"));
         Assert.assertEquals(strat.getItemIdentifier(item2), "uk-id");
     }
+    
+    @Test
+    public void withRegistrationAuthority() throws Exception {
+        final UKItemIdentificationStrategy strat = makeStrat();
+        final DomElementItem item = makeItem("present");
+
+        performExtractions(item);
+        
+        final ClassToInstanceMultiMap<ItemMetadata> metadata = item.getItemMetadata();
+        final List<ErrorStatus> errors = metadata.get(ErrorStatus.class);
+        Assert.assertEquals(errors.size(), 0);
+        
+        Assert.assertEquals(strat.getItemIdentifier(item), "uk002232 (http://ukfederation.org.uk)");
+    }
+    
+    @Test
+    public void withoutRegistrationAuthority() throws Exception {
+        final UKItemIdentificationStrategy strat = makeStrat();
+        final DomElementItem item = makeItem("absent");
+
+        performExtractions(item);
+        
+        final ClassToInstanceMultiMap<ItemMetadata> metadata = item.getItemMetadata();
+        final List<ErrorStatus> errors = metadata.get(ErrorStatus.class);
+        Assert.assertEquals(errors.size(), 0);
+        
+        Assert.assertEquals(strat.getItemIdentifier(item), "uk002232");
+    }
+    
+    @Test
+    public void ignoredAuthority() throws Exception {
+        final UKItemIdentificationStrategy strat = makeStrat();
+        final Set<String> auths = new HashSet<String>();
+        auths.add("http://ukfederation.org.uk");
+        strat.setIgnoredAuthorities(auths);
+        
+        final DomElementItem item = makeItem("present");
+
+        performExtractions(item);
+        
+        final ClassToInstanceMultiMap<ItemMetadata> metadata = item.getItemMetadata();
+        final List<ErrorStatus> errors = metadata.get(ErrorStatus.class);
+        Assert.assertEquals(errors.size(), 0);
+        
+        Assert.assertEquals(strat.getItemIdentifier(item), "uk002232");
+    }
+    
+    @Test
+    public void mappedAuthority() throws Exception {
+        final UKItemIdentificationStrategy strat = makeStrat();
+        final Map<String, String> nameMap = new HashMap<String, String>();
+        nameMap.put("http://ukfederation.org.uk", "UKf");
+        strat.setDisplayNames(nameMap);
+        
+        final DomElementItem item = makeItem("present");
+
+        performExtractions(item);
+        
+        final ClassToInstanceMultiMap<ItemMetadata> metadata = item.getItemMetadata();
+        final List<ErrorStatus> errors = metadata.get(ErrorStatus.class);
+        Assert.assertEquals(errors.size(), 0);
+        
+        Assert.assertEquals(strat.getItemIdentifier(item), "uk002232 (UKf)");
+    }
+    
 }

0cabbe92086c296c74d56e0f3f8c6b2179a4ec65
Ian Young <ian@iay.org.uk>
2013-09-20 15:06:12 +0100

    Fix typo in Javadoc.

--- src/main/java/uk/org/ukfederation/mda/dom/ElementWhitespaceTrimmingStage.java
+++ src/main/java/uk/org/ukfederation/mda/dom/ElementWhitespaceTrimmingStage.java
@@ -18,7 +18,7 @@ package uk.org.ukfederation.mda.dom;
 
 /**
  * Stage to trim leading and trailing whitespace from the text content of named elements
- * within a {@link DomDocumentItem}.
+ * within a {@link DomElementItem}.
  */
 public class ElementWhitespaceTrimmingStage extends AbstractElementVisitingStage {
 

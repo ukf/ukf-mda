65aa63aefc9461bb1c4220bfc969b316f95502c0
Ian Young <ian@iay.org.uk>
2013-11-13 18:25:27 +0000

    Generic X.509 certificate validation stage.

---
+++ src/main/java/uk/org/ukfederation/mda/validate/X509CertificateValidationStage.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2013 University of Edinburgh.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.org.ukfederation.mda.validate;
+
+import java.io.ByteArrayInputStream;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+
+import javax.annotation.Nonnull;
+
+import net.shibboleth.metadata.ErrorStatus;
+import net.shibboleth.metadata.Item;
+import net.shibboleth.utilities.java.support.codec.Base64Support;
+import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
+
+import org.w3c.dom.Element;
+
+/**
+ * Stage to apply a collection of validators to each X.509 certificate in metadata.
+ */ 
+public class X509CertificateValidationStage extends AbstractValidationStage<X509Certificate> {
+
+    /** Certificate factory to use to convert to X.509 certificates. */
+    private CertificateFactory factory;
+    
+    /** {@inheritDoc} */
+    protected boolean applicable(@Nonnull final Element e) {
+        return XMLDSIGSupport.XML_DSIG_NS.equals(e.getNamespaceURI()) &&
+                "X509Certificate".equals(e.getLocalName());
+    }
+
+    /** {@inheritDoc} */
+    protected void visit(@Nonnull final Element element, @Nonnull final Item<Element> item) {
+        final String text = element.getTextContent();
+        final byte[] data = Base64Support.decode(text);
+        try {
+            final X509Certificate cert =
+                    (X509Certificate) factory.generateCertificate(new ByteArrayInputStream(data));
+            applyValidators(cert, item);
+        } catch (CertificateException e) {
+            item.getItemMetadata().put(new ErrorStatus(getId(), "could not convert X509Certificate data"));
+        }
+    }
+
+    /** {@inheritDoc} */
+    protected void doInitialize() throws ComponentInitializationException {
+        super.doInitialize();
+        try {
+            factory = CertificateFactory.getInstance("X.509");
+        } catch (CertificateException e) {
+            throw new ComponentInitializationException("can't create X.509 certificate factory", e);
+        }
+    }
+
+    /** {@inheritDoc} */
+    protected void doDestroy() {
+        factory = null;
+        super.doDestroy();
+    }
+
+}
---
+++ src/test/java/uk/org/ukfederation/mda/validate/X509CertificateValidationStageTest.java
@@ -0,0 +1,118 @@
+
+package uk.org.ukfederation.mda.validate;
+
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import junit.framework.Assert;
+import net.shibboleth.metadata.ErrorStatus;
+import net.shibboleth.metadata.Item;
+import net.shibboleth.metadata.WarningStatus;
+import net.shibboleth.metadata.dom.DOMElementItem;
+import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
+import net.shibboleth.utilities.java.support.xml.XMLParserException;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.w3c.dom.Element;
+
+import uk.org.ukfederation.mda.BaseDOMTest;
+
+public class X509CertificateValidationStageTest extends BaseDOMTest {
+
+    @BeforeClass
+    public void beforeClass() throws Exception {
+        setTestingClass(X509CertificateValidationStage.class);
+    }
+    
+    private X509CertificateValidationStage makeStage() throws ComponentInitializationException {
+        final X509CertificateValidationStage stage = new X509CertificateValidationStage();
+        stage.setId("test");
+        return stage; 
+    }
+    
+    private DOMElementItem makeItem(final String which) throws XMLParserException {
+        final Element doc = readXmlData(classRelativeResource(which));
+        return new DOMElementItem(doc);
+    }
+    
+    private void errorsAndWarnings(final Item<Element> item,
+            final int expectedErrors, final int expectedWarnings) {
+        final Collection<ErrorStatus> errors = item.getItemMetadata().get(ErrorStatus.class);
+        Assert.assertEquals(expectedErrors, errors.size());
+        final Collection<WarningStatus> warnings = item.getItemMetadata().get(WarningStatus.class);
+        Assert.assertEquals(expectedWarnings, warnings.size());
+        //for (ErrorStatus err: errors) {
+        //    System.out.println("Error: " + err.getComponentId() + ": " + err.getStatusMessage());
+        //}
+        //for (WarningStatus warn: warnings) {
+        //    System.out.println("Warning: " + warn.getComponentId() + ": " + warn.getStatusMessage());
+        //}
+    }
+
+    @Test
+    public void testNothing() throws Exception {
+        final DOMElementItem item = makeItem("in.xml");
+        
+        final List<Item<Element>> items = new ArrayList<>();
+        items.add(item);
+        
+        final X509CertificateValidationStage stage = makeStage();
+        // not setting any validators to run
+        stage.initialize();
+        
+        stage.execute(items);
+
+        errorsAndWarnings(item, 0, 0);
+    }
+    
+    @Test
+    public void testError() throws Exception {
+        final DOMElementItem item = makeItem("in.xml");
+        
+        final List<Item<Element>> items = new ArrayList<>();
+        items.add(item);
+        
+        final X509CertificateRSAKeyLengthValidator val =
+                new X509CertificateRSAKeyLengthValidator();
+        val.setErrorBoundary(2049);
+        
+        final List<Validator<X509Certificate>> vals = new ArrayList<>();
+        vals.add(val);
+        
+        final X509CertificateValidationStage stage = makeStage();
+        stage.setValidators(vals);
+        stage.initialize();
+        
+        stage.execute(items);
+
+        errorsAndWarnings(item, 2, 0); // two identical certs, two errors
+    }
+    
+    @Test
+    public void testWarning() throws Exception {
+        final DOMElementItem item = makeItem("in.xml");
+        
+        final List<Item<Element>> items = new ArrayList<>();
+        items.add(item);
+        
+        final X509CertificateRSAKeyLengthValidator val =
+                new X509CertificateRSAKeyLengthValidator();
+        val.setWarningBoundary(2049);
+        val.setErrorBoundary(2048);
+        
+        final List<Validator<X509Certificate>> vals = new ArrayList<>();
+        vals.add(val);
+        
+        final X509CertificateValidationStage stage = makeStage();
+        stage.setValidators(vals);
+        stage.initialize();
+        
+        stage.execute(items);
+        
+        errorsAndWarnings(item, 0, 2); // two identical certs, two warnings
+    }
+    
+}

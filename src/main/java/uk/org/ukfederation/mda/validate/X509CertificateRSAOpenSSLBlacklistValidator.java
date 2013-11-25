e02ec096c0c60bb20f89de8951f440a29982ee37
Ian Young <ian@iay.org.uk>
2013-11-25 10:07:58 +0000

    Make an error message more generic.

--- src/main/java/uk/org/ukfederation/mda/validate/X509CertificateRSAOpenSSLBlacklistValidator.java
+++ src/main/java/uk/org/ukfederation/mda/validate/X509CertificateRSAOpenSSLBlacklistValidator.java
@@ -174,7 +174,7 @@ public class X509CertificateRSAOpenSSLBlacklistValidator extends AbstractX509Cer
             if (keySize == 0 || keySize == modulus.bitLength()) {
                 final String value = openSSLDigest(modulus);
                 if (blacklistedValues.contains(value)) {
-                    addError("RSA modulus included in weak key blacklist", item, stageId);
+                    addError("RSA modulus included in key blacklist", item, stageId);
                 }
             }
         }

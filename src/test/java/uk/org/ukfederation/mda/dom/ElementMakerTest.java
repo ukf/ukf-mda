9b3f3b2fb4d9fa80717010d5a196b62180039284
Ian Young <ian@iay.org.uk>
2015-01-24 17:55:33 +0000

    Element maker for the Container system.

---
+++ src/test/java/uk/org/ukfederation/mda/dom/ElementMakerTest.java
@@ -0,0 +1,35 @@
+
+package uk.org.ukfederation.mda.dom;
+
+import javax.xml.namespace.QName;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import uk.org.ukfederation.mda.BaseDOMTest;
+
+import com.google.common.base.Function;
+
+public class ElementMakerTest extends BaseDOMTest {
+
+    private final Document doc;
+    
+    protected ElementMakerTest() throws Exception {
+        super(ElementMaker.class);
+        setUp();
+        doc = getParserPool().newDocument();
+    }
+    
+    @Test
+    public void apply() {
+        final Function<Container, Element> maker = new ElementMaker(new QName("ns", "local"));
+        final Element root = doc.createElementNS("ns", "root");
+        final Container rootContainer = new Container(root);
+        final Element newElement = maker.apply(rootContainer);
+        Assert.assertNotNull(newElement);
+        Assert.assertEquals(newElement.getLocalName(), "local");
+        Assert.assertEquals(newElement.getNamespaceURI(), "ns");
+    }
+}
